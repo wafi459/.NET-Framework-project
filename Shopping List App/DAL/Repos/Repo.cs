858822cdb8a6cde478a5bc8e885511df58fe3401
wafi459 <miuuuu092@gmail.com>
2025-05-10 15:00:54 +0600

    Update Repo.cs

--- Shopping List App/DAL/Repos/Repo.cs
+++ Shopping List App/DAL/Repos/Repo.cs
@@ -1,1 +1,19 @@
+using DAL.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repos
+{
+    internal class Repo
+    {
+        protected ShoppingContext db;
+        internal Repo()
+        {
+            db = new ShoppingContext();
+        }
+    }
+}
 
