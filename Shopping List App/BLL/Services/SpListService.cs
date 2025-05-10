ea7c020727e8695601cbc3a9a03d20ba05abda81
wafi459 <miuuuu092@gmail.com>
2025-05-10 15:03:48 +0600

    Add files via upload

---
+++ Shopping List App/BLL/Services/SpListService.cs
@@ -0,0 +1,53 @@
+﻿using AutoMapper;
+using BLL.DTOs;
+using DAL;
+using DAL.EF.Tables;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Services
+{
+    public class SpListService
+    {
+        public static Mapper GetMapper()
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<Item, ItemDTO>();
+                cfg.CreateMap<ItemDTO, Item>();
+            });
+            return new Mapper(config);
+        }
+
+        public static ItemDTO Add(ItemDTO item)
+        {
+            var repo = DataAccessFactory.ListData();
+
+            var itemEntity = GetMapper().Map<Item>(item);
+
+         
+            var create = repo.Add(itemEntity);
+
+            
+            return GetMapper().Map<ItemDTO>(create);
+        }
+
+        public static List<ItemDTO> GetItems()
+        {
+            var repo = DataAccessFactory.ListData();
+
+            var items = repo.GetAll();
+            return GetMapper().Map<List<ItemDTO>>(items);
+        }
+
+        public static bool Delete(int id)
+        {
+            var repo = DataAccessFactory.ListData();
+
+            return repo.Delete(id);
+        }
+    }
+}
---
+++ Shopping List App/BLL/Services/UserService.cs
@@ -0,0 +1,50 @@
+﻿using AutoMapper;
+using BLL.DTOs;
+using DAL.EF.Tables;
+using DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Services
+{
+    public class UserService
+    {
+        public static Mapper GetMapper()
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<User, UserDTO>();
+                cfg.CreateMap<UserDTO, User>();
+                cfg.CreateMap<UserLoginDTO, User>();
+            });
+            return new Mapper(config);
+        }
+
+        public static UserDTO Register(UserDTO user)
+        {
+            var repo = DataAccessFactory.UserData();
+
+          
+            var userEntity = GetMapper().Map<User>(user);
+
+            var createdUser = repo.Add(userEntity);
+
+           
+            return GetMapper().Map<UserDTO>(createdUser);
+        }
+
+        public static UserDTO Login(UserLoginDTO loginDTO)
+        {
+            var repo = DataAccessFactory.UserData();
+
+           
+            var user = repo.GetByUsername(loginDTO.Username);
+
+            
+            return GetMapper().Map<UserDTO>(user);
+        }
+    }
+}
