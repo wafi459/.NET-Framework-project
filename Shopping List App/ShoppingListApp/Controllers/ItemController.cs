df29d1d3d686c6bf43e2080347f6b224713e5b25
wafi459 <miuuuu092@gmail.com>
2025-05-10 15:06:03 +0600

    Add files via upload

---
+++ Shopping List App/ShoppingListApp/Controllers/ItemController.cs
@@ -0,0 +1,46 @@
+﻿using BLL.DTOs;
+using BLL.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ShoppingListApp.Controllers
+{
+    public class ItemController : ApiController
+    {
+        [HttpGet]
+        [Route("api/shoppinglist/items")]
+        public HttpResponseMessage Getall()
+        {
+            var data = SpListService.GetItems();
+            return Request.CreateResponse(HttpStatusCode.OK, data);
+        }
+
+
+       
+
+            [HttpPost]
+            [Route("api/shoppinglist/items/add")]
+            public HttpResponseMessage Add(ItemDTO sl)
+            {
+                SpListService.Add(sl);
+                return Request.CreateResponse(HttpStatusCode.OK);
+            }
+
+
+
+           
+            [HttpDelete]
+            [Route("api/shoppinglist/items/delete/{id}")]
+            public HttpResponseMessage Delete(int id)
+            {
+                var isDeleted = SpListService.Delete(id);
+               
+
+                return Request.CreateResponse(HttpStatusCode.OK, "deleted");
+            }
+        }
+}
