b27e0be171f09fd6c025d3f94e46729c961415b3
wafi459 <miuuuu092@gmail.com>
2025-05-10 15:03:02 +0600

    Add files via upload

---
+++ Shopping List App/DAL/EF/Tables/Item.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.EF.Tables
+{
+    public class Item
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public float Price { get; set; }
+        public int Quantity { get; set; }
+        public string Category { get; set; }
+
+        [ForeignKey("SList")]
+       public int ListId { get; set; }
+        public virtual SpList SList { get; set; }
+
+    }
+}
---
+++ Shopping List App/DAL/EF/Tables/SpList.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.EF.Tables
+{
+    public class SpList
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        [ForeignKey("User")]
+        public int UserId { get; set; }
+        public virtual User User { get; set; }
+
+        
+        public virtual ICollection<Item> Items { get; set; }
+        public SpList()
+        {
+            Items = new List<Item>();
+        }
+    }
+}
