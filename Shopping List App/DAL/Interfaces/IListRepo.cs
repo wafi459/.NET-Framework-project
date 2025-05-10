267e7a39cfc68e336835b5f407c80c68f371bd53
wafi459 <miuuuu092@gmail.com>
2025-05-10 14:58:00 +0600

    Add files via upload

---
+++ Shopping List App/DAL/Interfaces/IListRepo.cs
@@ -0,0 +1,16 @@
+﻿using DAL.EF.Tables;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Interfaces
+{
+    public interface IListRepo
+    {
+        Item Add(Item item);
+        List<Item> GetAll();
+        bool Delete(int id);
+    }
+}
---
+++ Shopping List App/DAL/Interfaces/IUserRepo.cs
@@ -0,0 +1,16 @@
+﻿using DAL.EF.Tables;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Interfaces
+{
+    public interface IUserRepo
+    {
+        User Add(User user);
+        User GetByUsername(string username);
+        User GetByEmail(string email);
+    }
+}
